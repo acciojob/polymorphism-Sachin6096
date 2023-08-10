5cfa97a3f38c6c8c9ea788614b40603b9889ebf6
sachin6096 <sachinaryaa29@gmail.com>
2023-08-10 15:41:44 +0530

    first attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,31 @@ package com.driver;
 
 public class Main {
 
+    public static class Product{
+
+        public int production(int x, int y)
+        {
+            return x*y;
+        }
+
+        public int production(int x, int y, int z)
+        {
+            return x*y*z;
+        }
+
+        public double production(double x, double y)
+        {
+            return x*y;
+        }
+    }
+
+
+    public static void main(String[] args)
+    {
+        Product obj = new Product();
+
+        int a = obj.production(5,6);
+        int b = obj.production(5,6,4);
+        double c = obj.production(5.0,6.2);
+    }
 }
